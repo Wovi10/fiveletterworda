38c3efb8d24b8ad67b65dc51e40aa725407f3a48
Wout Vinckevleugel <wout.vinck@gmail.com>
2023-06-11 15:48:51 +0200

    Already found a 4 word solution

--- my_version.py
+++ my_version.py
@@ -128,17 +128,36 @@ def get_all_words():
         all_words_in_list = list(word_file.read().split())
 
     print(f"There are {len(all_words_in_list)} words in total.")
-    return all_words_in_list
 
-def all_word_length_words(all_words_correct_length):
-    valid_words = []
-    for word_to_check in all_words_correct_length:
+    right_length = []
+    for word_to_check in all_words_in_list:
         if len(word_to_check) != WORD_LENGTH:
             continue
-        valid_words.append(word_to_check)
+        right_length.append(word_to_check)
 
-    print(f"There are {len(valid_words)} words with the right length.")
-    return valid_words
+    print(f"There are {len(right_length)} words with the right length.")
+
+    valid_words = []
+    for word in right_length:
+        is_valid = check_word(word)
+        if is_valid:
+            valid_words.append(word)
+
+    print(f"There are {len(valid_words)} words without repeating letters.")
+
+    no_anagrams = []
+    sorted_words = []
+    for word in valid_words:
+        sorted_word = ''.join(sorted(word))
+        if sorted_word in sorted_words:
+            continue
+        sorted_words.append(sorted_word)
+        no_anagrams.append(word)
+
+    print(f"There are {len(no_anagrams)} words that aren't anagrams.")
+
+    return no_anagrams
+
 
 def check_word(word):
     temp_used_letter = []
@@ -148,31 +167,37 @@ def check_word(word):
         temp_used_letter.append(letter)
     return True
 
-def all_different_letter_words(words):
-    valid_words = []
-    for word in words:
-        is_valid = check_word(word)
-        if is_valid:
-            valid_words.append(word)
+def check_word_to_other_word(used_letters, word2):
+    for letter in word2:
+        if letter in used_letters:
+            return False
+    return True
 
-    print(f"There are {len(valid_words)} words without repeating letters.")
-    return valid_words
+def main_again():
+    longest_row = []
+    longest_rows = []
+    all_words_to_check = get_all_words()
+    index = 1
+    for word in all_words_to_check:
+        if index % 200 == 0:
+            print(f"Tested {index} words.")
+            print("Longest row so far is:")
+            for word in longest_row:
+                print(word)
+        used_letters = []
+        possibility = []
+        possibility.append(word)
+        for letter in word:
+            used_letters.append(letter)
+        for word2 in all_words_to_check:
+            is_valid = check_word_to_other_word(used_letters, word2)
+            if not is_valid:
+                continue
+            possibility.append(word2)
+            for letter in word2:
+                used_letters.append(letter)
+            if len(possibility) > len(longest_row):
+                longest_row = possibility
+        index += 1
 
-def no_angram_words(words):
-    no_anagrams = []
-    sorted_words = []
-    for word in words:
-        sorted_word = ''.join(sorted(word))
-        if sorted_word in sorted_words:
-            continue
-        sorted_words.append(sorted_word)
-        no_anagrams.append(word)
-
-    print(f"There are {len(no_anagrams)} words that aren't anagrams.")
-    return no_anagrams
-
-
-all_words = get_all_words()
-correct_length = all_word_length_words(all_words)
-different_letters = all_different_letter_words(correct_length)
-no_angram_words(different_letters)
+main_again()
