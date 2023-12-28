c3e454aa1c6663dfcfca59708052ca0d554dbbc9
Wout Vinckevleugel <wout.vinck@gmail.com>
2023-12-28 17:29:04 +0100

    jups

--- FiveLetterWordCSharp/FiveLetterWordCSharp/Program.cs
+++ FiveLetterWordCSharp/FiveLetterWordCSharp/Program.cs
@@ -5,8 +5,8 @@ internal static class Program
     private const int WordLength = 5;
     private const int CheckingStep = 1000;
 
-    private static readonly char[] CustomOrder2 = {'e', 's', 'i', 'a', 'r', 'n', 't', 'o', 'l', 'c', 'd', 'u', 'g', 'p', 'm', 
-        'h', 'b', 'y', 'f', 'v', 'k', 'w', 'z', 'x', 'j', 'q'};
+    // private static readonly char[] CustomOrder2 = {'e', 's', 'i', 'a', 'r', 'n', 't', 'o', 'l', 'c', 'd', 'u', 'g', 'p', 'm', 
+    //     'h', 'b', 'y', 'f', 'v', 'k', 'w', 'z', 'x', 'j', 'q'};
     private static readonly char[] CustomOrder = {'q', 'j', 'x', 'z', 'w', 'k', 'v', 'f', 'y', 'b', 'h', 'm', 'p', 'g', 'u', 
         'd', 'c', 'l', 'o', 't', 'n', 'r', 'a', 'i', 's', 'e'};
 
@@ -17,17 +17,20 @@ internal static class Program
 
     private static void Main()
     {
-        GetWords();
+        var startTime = DateTime.Now;
+        _allWords = GetWords();
         MakeAllCombinations();
+        Console.WriteLine("All possible combinations we found.");
         foreach (var combination in AllCombinations)
         {
             Console.WriteLine(string.Join(", ", combination));
         }
 
         Console.WriteLine("====== Finished running. ======");
+        Console.WriteLine($"This run took {CalculateTimePassed(startTime, DateTime.Now)}");
     }
 
-    private static void GetWords()
+    private static List<string> GetWords()
     {
         var filteringStartTime = DateTime.Now;
         var filePath = Path.Combine("../../..", "Files/words_alpha.txt");
@@ -43,8 +46,8 @@ internal static class Program
             if (wordToCheck.Length != WordLength)
                 continue;
 
-            var isValid = CheckWord(wordToCheck);
-            if (!isValid)
+            var hasRepeatingLetter = HasRepeatingLetter(wordToCheck);
+            if (!hasRepeatingLetter)
                 continue;
 
             var sortedWord = SortString(wordToCheck);
@@ -61,10 +64,11 @@ internal static class Program
         var sortedList = correctWords.OrderBy(word => word, new CustomOrderComparer()).ToList();
         Console.WriteLine("Sorted according to custom order");
         Console.WriteLine($"Created word list in {CalculateTimePassed(filteringStartTime, DateTime.Now)}");
-        _allWords = sortedList;
+
+        return sortedList;
     }
 
-    static bool CheckWord(string word)
+    private static bool HasRepeatingLetter(string word)
     {
         var tempUsedLetter = new List<char>();
         foreach (var letter in word.OrderBy(c => c))
@@ -78,71 +82,62 @@ internal static class Program
 
     private static bool CheckWordToOtherWord(string usedLetters, string word)
     {
-        var sortedWord = new string(word.OrderBy(character => CustomOrder.ToList().IndexOf(character)).ToArray());
-        var sortedUsedLetters = new string(usedLetters.OrderBy(c => CustomOrder.ToList().IndexOf(c)).ToArray());
-        return sortedWord.All(letter => !sortedUsedLetters.Contains(letter));
+        return word.All(letter => !usedLetters.Contains(letter));
     }
 
     private static void MakeAllCombinations()
     {
-        WordLoop("", new List<string>(), new List<string>());
+        WordLoop(string.Empty, new List<string>(), new List<string>(), _allWords!.GroupBy(x => x[0]));
     }
 
-    private static List<string> WordLoop(string previousTriedGroups, List<string> previousWords, List<string> possibility, int depth = 1, int counter = 0)
+    private static List<string> WordLoop(string previousTriedGroups, List<string> previousWords, List<string> possibility, IEnumerable<IGrouping<char, string>> allGroups, int depth = 1, int counter = 0)
     {
-        if (_allWords == null) return possibility;
-        var groups = _allWords.GroupBy(x => x[0]);
-        var triedGroups = "";
-        foreach (var group in groups)
+        if (_allWords == null) 
+            return possibility;
+
+        var groups = _allWords.GroupBy(x => x[0]).Where(group => !previousTriedGroups.Contains(group.Key));
+        var triedGroups = string.Empty;
+
+        foreach (var group in allGroups)
         {
             var currentLetter = group.Key;
             triedGroups += currentLetter;
-            if (depth == 1 && currentLetter != 'f')
-                continue;
-            if (previousTriedGroups.Contains(currentLetter))
+            if (previousTriedGroups.Contains(currentLetter) || string.Join("", previousWords).Distinct().Contains(currentLetter))
                 continue;
 
             var groupList = group.ToList();
             foreach (var word in groupList)
             {
-                if ((depth == 1 && word != "fjord") || (depth == 2 && word != "gucks") || (depth == 3 && word != "nymph") || (depth == 4 && word != "vibex"))
-                    continue;
-                if (previousWords.Count == depth)
-                {
-                    previousWords.RemoveAt(previousWords.Count - 1);
-                }
                 if (depth == 1)
                 {
                     possibility.Clear();
                     previousWords.Clear();
                 }
+
+                if (previousWords.Count == depth) 
+                    previousWords.RemoveAt(previousWords.Count - 1);
+
                 var (usedLetters, newPossibility) = DefaultLoopActions(word, previousWords, possibility);
                 possibility = newPossibility;
-                if (usedLetters == "")
-                {
+                if (usedLetters == string.Empty)
                     continue;
-                }
+
                 previousWords.Add(word);
                 var words = new List<string>(previousWords);
-                if (depth == 4 && word == "vibex")
-                {
-                    Console.WriteLine();
-                }
                 if (words.Count == MaxNumWords)
                 {
-                    Console.WriteLine("WOOHOOOOOW");
-                    Console.WriteLine(string.Join(", ", words));
+                    Console.WriteLine("Found one");
                     AllCombinations.Add(words);
                     return possibility;
                 }
-                possibility = WordLoop(triedGroups, words, possibility, depth + 1, counter);
-                if (depth != 1) continue;
+                possibility = WordLoop(triedGroups, words, possibility, groups.Where(thingy => thingy.Key != currentLetter), depth + 1, counter);
+                if (depth != 1) 
+                    continue;
+
                 counter++;
                 if (counter % CheckingStep == 0)
-                {
                     Console.WriteLine($"Found {counter - CheckingStep}-{counter} in " +
                                       $"{(DateTime.Now - DateTime.Today.AddHours(counter))}");
-                }
             }
         }
 
@@ -178,11 +173,8 @@ internal static class Program
 
     private static string SortString(string input)
     {
-        char[] customOrder = {'q', 'j', 'x', 'z', 'w', 'k', 'v', 'f', 'y', 'b', 'h', 'm', 'p', 'g', 'u', 
-            'd', 'c', 'l', 'o', 't', 'n', 'r', 'a', 'i', 's', 'e'};
-        var chars = input.ToCharArray()
-            .OrderBy(c => Array.IndexOf(customOrder, c))
-            .ToArray();
+        var chars = input.ToCharArray();
+        Array.Sort(chars, (a,b) => Array.IndexOf(CustomOrder, a).CompareTo(Array.IndexOf(CustomOrder, b)));
         
         return new string(chars);
     }
--- FiveLetterWordCSharp/FiveLetterWordCSharp/obj/Debug/net8.0/FiveLetterWordCSharp.AssemblyInfo.cs
+++ FiveLetterWordCSharp/FiveLetterWordCSharp/obj/Debug/net8.0/FiveLetterWordCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FiveLetterWordCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+332f89f99d1e27ea4b66a76134a8147a2cd2d1d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91e40f7e5778ffb92cd58ebca2cfbd75044783e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FiveLetterWordCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FiveLetterWordCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
